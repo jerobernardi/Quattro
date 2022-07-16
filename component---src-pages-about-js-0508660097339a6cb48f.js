2eaf0394d089708a7cbe4c208b5e7d410be170bf
jero <jerobernardi@gmail.com>
2022-07-16 11:59:39 -0300

    Updates

---
+++ component---src-pages-about-js-0508660097339a6cb48f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquattro_vidrios=self.webpackChunkquattro_vidrios||[]).push([[682],{5465:function(e,t,a){a.r(t);var l=a(7294),u=(a(1597),a(8193));t.default=function(){return l.createElement(u.Z,{pageTitle:"About"},l.createElement("p",null,"Blablablal"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-0508660097339a6cb48f.js.map
